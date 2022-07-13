f43d91f86e124651bc9c5b340b2be2ae55fdbaae
SRAZKVT <sarah.mathey66666@gmail.com>
2022-07-13 03:58:31 +0200

    fixed GoingToTheCinema.hs

--- solutions/haskell/GoingToTheCinema.hs
+++ solutions/haskell/GoingToTheCinema.hs
@@ -1,11 +1,8 @@
-import Debug.Trace
-
-calc :: Int -> Int -> Double -> Int -> Int
-calc card ticket perc n = if (calcCard > calcTicket) then (calc card ticket perc (n + 1)) else n
+calc :: Int -> Int -> Double -> Int -> Double-> Int
+calc card ticket perc n cache = if (calcCard >= calcTicket) then (calc card ticket perc (n + 1) (cache + cardTicketCalc n)) else n
     where calcTicket = ticket * n
-          calcCard = ceiling (fromIntegral card + (cardTicketCalc n))
-          cardTicketCalc 0 = 0
-          cardTicketCalc x = fromIntegral ticket * (perc^x) + cardTicketCalc (x - 1)
+          calcCard = ceiling (fromIntegral card + (cache + (cardTicketCalc n)))
+          cardTicketCalc x = fromIntegral ticket * (perc^x)
 
 movie :: Int -> Int -> Double -> Int
-movie card ticket perc = (traceShow [show card,show ticket,show perc]) calc card ticket perc 1
+movie card ticket perc = calc card ticket perc 1 0.0
